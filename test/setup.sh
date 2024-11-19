90ab868af7809b7863642d106f93ae66de8484ec
David Griffin <david.griffin@fauna.com>
2024-11-19 15:44:01 -0800

    Setup script fixes.

--- test/setup.sh
+++ test/setup.sh
@@ -1,10 +1,16 @@
+#! /bin/sh
+
 DB_NAME="ECommerceJava"
 LOCAL_ENDPOINT="http://localhost:8443/"
 SECRET="secret"
 
 cp ./test/local-project .fauna-project
 
+echo "Copied .fauna-project"
+
 fauna endpoint add local --url "$LOCAL_ENDPOINT" --secret "$SECRET"
+echo "Added local endpoint"
 fauna endpoint select local
+echo "Selected local endpoint"
 
 fauna create-database "$DB_NAME"
