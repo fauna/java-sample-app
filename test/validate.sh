6c29a6eeb4a400699bca7922cc5031056fe397e9
David Griffin <david.griffin@fauna.com>
2024-11-20 13:52:36 -0800

    Add retries to furst curl command, to allow for bootRun to finish.

--- test/validate.sh
+++ test/validate.sh
@@ -2,6 +2,8 @@
 
 ENDPOINT="http://localhost:8080"
 
-curl -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer secret" "$ENDPOINT/products?pageSize=1"
+curl -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer secret" \
+    --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 1 --retry-max-time 60 \
+    --retry-all-errors "$ENDPOINT/products?pageSize=1"
 
 
